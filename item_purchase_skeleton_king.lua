36f3ddbff2b49e2cc913e189020515eefb8f6514
Ilya Shpigor <petrsum@gmail.com>
2017-12-04 21:07:56 +0100

    Add the skeleton_king items

---
+++ item_purchase_skeleton_king.lua
@@ -0,0 +1,41 @@
+package.path = package.path .. ";../?.lua"
+local item_purchase_utility = require("item_purchase_utility")
+
+local itemsToBuy = {
+    "item_tango",
+    "item_stout_shield",
+    "item_branches",
+    "item_branches",
+    "item_boots",
+    "item_magic_stick",
+    "item_enchanted_mango",
+    "item_belt_of_strength",
+    "item_gloves",
+
+    "item_broadsword",
+    "item_robe",
+    "item_chainmail",
+
+    "item_belt_of_strength",
+    "item_ogre_axe",
+    "item_recipe_sange",
+    "item_talisman_of_evasion",
+
+    "item_platemail",
+    "item_chainmail",
+    "item_hyperstone",
+    "item_recipe_assault",
+
+    "item_point_booster",
+    "item_staff_of_wizardry",
+    "item_ogre_axe",
+    "item_blade_of_alacrity",
+
+    "item_vitality_booster",
+    "item_vitality_booster",
+    "item_reaver",
+};
+
+function ItemPurchaseThink()
+    item_purchase_utility.PurchaseItem(itemsToBuy)
+end
