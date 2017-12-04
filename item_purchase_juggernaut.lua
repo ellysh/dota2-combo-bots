2594965a522d67d625b923d13c10970c21fbab63
Ilya Shpigor <petrsum@gmail.com>
2017-12-04 20:41:53 +0100

    Initial add the item purchase for Juggernaut

---
+++ item_purchase_juggernaut.lua
@@ -0,0 +1,79 @@
+local tableItemsToBuy = {
+    "item_flask",
+    "item_tango",
+    "item_branches",
+    "item_branches",
+    "item_stout_shield",
+    "item_orb_of_venom",
+
+    "item_magic_stick",
+    "item_enchanted_mango",
+
+    "item_boots",
+    "item_blades_of_attack",
+    "item_blades_of_attack",
+
+    "item_slippers",
+    "item_circlet",
+    "item_recipe_wraith_band",
+    "item_ring_of_protection",
+    "item_sobi_mask",
+
+    "item_blade_of_alacrity",
+    "item_boots_of_elves",
+    "item_recipe_yasha",
+
+    "item_ultimate_orb",
+    "item_recipe_manta",
+
+    "item_blade_of_alacrity",
+    "item_blade_of_alacrity",
+    "item_robe",
+    "item_recipe_diffusal_blade",
+
+    "item_point_booster",
+    "item_staff_of_wizardry",
+    "item_ogre_axe",
+    "item_blade_of_alacrity",
+
+    "item_javelin",
+    "item_belt_of_strength",
+    "item_recipe_basher",
+
+    "item_ring_of_health",
+    "item_vitality_booster",
+    "item_stout_shield",
+
+    "item_recipe_abyssal_blade",
+
+    "item_quarterstaff",
+    "item_eagle",
+    "item_talisman_of_evasion",
+};
+
+
+----------------------------------------------------------------------------------------------------
+
+function ItemPurchaseThink()
+
+    local npcBot = GetBot();
+
+    if ( #tableItemsToBuy == 0 )
+    then
+        npcBot:SetNextItemPurchaseValue( 0 );
+        return;
+    end
+
+    local sNextItem = tableItemsToBuy[1];
+
+    npcBot:SetNextItemPurchaseValue( GetItemCost( sNextItem ) );
+
+    if ( npcBot:GetGold() >= GetItemCost( sNextItem ) )
+    then
+        npcBot:ActionImmediate_PurchaseItem( sNextItem );
+        table.remove( tableItemsToBuy, 1 );
+    end
+
+end
+
+----------------------------------------------------------------------------------------------------
