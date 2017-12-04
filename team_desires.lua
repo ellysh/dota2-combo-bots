05b62b3aaeba3f6a2775279ecf40a1132897c82a
Ilya Shpigor <petrsum@gmail.com>
2017-12-04 20:33:35 +0100

    Fixes in the team_desires priorities

--- team_desires.lua
+++ team_desires.lua
@@ -4,7 +4,7 @@
 
 function UpdatePushLaneDesires()
 
-    return { 0.0, 0.5, 1.0 };
+    return { 0.5, 0.5, 0.5 };
 
 end
 
@@ -12,7 +12,7 @@ end
 
 function UpdateDefendLaneDesires()
 
-    return { 0.1, 0.2, 0.3 };
+    return { 0.2, 0.2, 0.2 };
 
 end
 
@@ -20,23 +20,24 @@ end
 
 function UpdateFarmLaneDesires()
 
-    return { 0.5, 0.6, 0.7 };
+    return { 0.5, 0.5, 0.5 };
 
 end
 
 ----------------------------------------------------------------------------------------------------
-
+--[[
 function UpdateRoamDesire()
 
     return { 0.5, GetTeamMember( 1 ) };
 
 end
+--]]
 
 ----------------------------------------------------------------------------------------------------
 
 function UpdateRoshanDesire()
 
-    return 0.8;
+    return 0.1;
 
 end
 
