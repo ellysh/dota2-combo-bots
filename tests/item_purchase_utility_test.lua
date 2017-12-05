28398ea66f7092c191631aee1525e32f0c4ca52a
Ilya Shpigor <petrsum@gmail.com>
2017-12-05 21:11:09 +0100

    Minor fixes in tests

--- tests/item_purchase_utility_test.lua
+++ tests/item_purchase_utility_test.lua
@@ -3,10 +3,13 @@ require("global_functions")
 local item_purchase_utility = require("item_purchase_utility")
 
 local itemsToBuy = {
-    "item_tango"
+    "item_tango",
+    "item_flask"
 };
 
 item_purchase_utility.PurchaseItem(itemsToBuy)
+item_purchase_utility.PurchaseItem(itemsToBuy)
 
--- Buy an item_tango with 150 gold cost at the beginning
-assert((TestBot.gold == 475), "item_purchase_utility.PurchaseItem() - failed")
+-- Buy an item_tango with 150+110 gold cost at the beginning
+assert((TestBot.gold == 365), "item_purchase_utility.PurchaseItem() - failed")
+assert((#itemsToBuy == 0), "item_purchase_utility.PurchaseItem() - failed")
