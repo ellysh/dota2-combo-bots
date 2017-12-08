74bf2fdcb009c6ea424760f1d5731ed790db295e
Ilya Shpigor <petrsum@gmail.com>
2017-12-08 22:45:50 +0100

    Fix the item list for Juggernaut

--- item_purchase_juggernaut.lua
+++ item_purchase_juggernaut.lua
@@ -2,43 +2,47 @@ local item_purchase_utility = require(
     GetScriptDirectory() .."/utility/item_purchase_utility")
 
 local itemsToBuy = {
+    -- Starting items
     "item_flask",
     "item_tango",
     "item_branches",
     "item_branches",
     "item_stout_shield",
-    "item_orb_of_venom",
+    "item_clarity",
 
+    -- Early game
+    "item_boots",
+    "item_wraith_band",
+    "item_ring_of_basilius",
+
+    -- Core items
+    -- Phase boots
+    "item_blades_of_attack",
+    "item_blades_of_attack",
+
+    -- Magic wand
     "item_magic_stick",
     "item_enchanted_mango",
 
-    "item_boots",
-    "item_blades_of_attack",
-    "item_blades_of_attack",
-
-    "item_slippers",
-    "item_circlet",
-    "item_recipe_wraith_band",
-    "item_ring_of_protection",
-    "item_sobi_mask",
-
+    -- Manta Style
     "item_blade_of_alacrity",
     "item_boots_of_elves",
     "item_recipe_yasha",
-
     "item_ultimate_orb",
     "item_recipe_manta",
 
+    -- Diffusial Blade
     "item_blade_of_alacrity",
     "item_blade_of_alacrity",
     "item_robe",
     "item_recipe_diffusal_blade",
 
-    "item_point_booster",
-    "item_staff_of_wizardry",
-    "item_ogre_axe",
-    "item_blade_of_alacrity",
+    -- Butterfly
+    "item_quarterstaff",
+    "item_eagle",
+    "item_talisman_of_evasion",
 
+    -- Abyssal Blade
     "item_javelin",
     "item_belt_of_strength",
     "item_recipe_basher",
@@ -47,11 +51,7 @@ local itemsToBuy = {
     "item_vitality_booster",
     "item_stout_shield",
 
-    "item_recipe_abyssal_blade",
-
-    "item_quarterstaff",
-    "item_eagle",
-    "item_talisman_of_evasion",
+    "item_recipe_abyssal_blade"
 };
 
 function ItemPurchaseThink()
