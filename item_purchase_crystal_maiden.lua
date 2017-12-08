6600e601d7165869153b51cdcf966986215b3c1a
Ilya Shpigor <petrsum@gmail.com>
2017-12-08 21:39:28 +0100

    Fix the item list for Crystam Mayden

--- item_purchase_crystal_maiden.lua
+++ item_purchase_crystal_maiden.lua
@@ -2,31 +2,50 @@ local item_purchase_utility = require(
     GetScriptDirectory() .."/utility/item_purchase_utility")
 
 local itemsToBuy = {
+    -- Starting items
     "item_tango",
     "item_clarity",
     "item_branches",
     "item_branches",
+
+    -- Core items
+    -- Tranquil boots
     "item_wind_lace",
     "item_boots",
+    "item_ring_of_regen",
+
+    "item_ward_observer",
+
+    -- Magic wand
     "item_magic_stick",
     "item_enchanted_mango",
-    "item_wind_lace",
-    "item_ring_of_regen",
+
+    -- Glimmer cape
     "item_cloak",
     "item_shadow_amulet",
+
+    "item_ward_observer",
+
+    -- Extension Items
+    -- Force Stuff
     "item_staff_of_wizardry",
-    "item_void_stone",
-    "item_recipe_cyclone",
+    "item_ring_of_health",
+    "item_recipe_force_staff",
+
+    -- Blink Dagger
+    "item_blink",
+
+    -- Lotus Orb
+    --"item_ring_of_health",
+    --"item_void_stone",
+    --"item_platemail",
+    --"item_energy_booster"
+
+    -- Aghanim Scepter
     "item_point_booster",
     "item_staff_of_wizardry",
     "item_ogre_axe",
-    "item_blade_of_alacrity",
-    "item_mithril_hammer",
-    "item_ogre_axe",
-    "item_recipe_black_king_bar",
-    "item_mystic_staff",
-    "item_ultimate_orb",
-    "item_void_stone",
+    "item_blade_of_alacrity"
 };
 
 function ItemPurchaseThink()
