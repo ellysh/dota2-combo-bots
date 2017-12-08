84f89b654f1c3db73b273f9ad68d75678b570ce7
Ilya Shpigor <petrsum@gmail.com>
2017-12-08 22:12:48 +0100

    Fix the item list for Shadow Shaman

--- item_purchase_shadow_shaman.lua
+++ item_purchase_shadow_shaman.lua
@@ -2,31 +2,48 @@ local item_purchase_utility = require(
     GetScriptDirectory() .."/utility/item_purchase_utility")
 
 local itemsToBuy = {
+    -- Starting items
+    "item_courier",
     "item_tango",
     "item_clarity",
     "item_branches",
     "item_branches",
+
+    -- Core items
+    -- Arcane boots
     "item_boots",
+    "item_energy_booster",
+
+    -- Magic wand
     "item_magic_stick",
     "item_enchanted_mango",
-    "item_energy_booster",
-    "item_void_stone",
-    "item_energy_booster",
-    "item_recipe_aether_lens",
-    "item_blink",
+
+    -- Force Stuff
     "item_staff_of_wizardry",
-    "item_void_stone",
-    "item_recipe_cyclone",
-    "item_wind_lace",
+    "item_ring_of_health",
+    "item_recipe_force_staff",
+
+    "item_ward_observer"
+
+    -- Aghanim Scepter
     "item_point_booster",
     "item_staff_of_wizardry",
     "item_ogre_axe",
-    "item_blade_of_alacrity",
+    "item_blade_of_alacrity"
+
+    -- Extension Items
+    -- Refresher Orb
     "item_ring_of_health",
     "item_void_stone",
     "item_ring_of_health",
     "item_void_stone",
-    "item_recipe_refresher",
+    "item_recipe_refresher"
+
+    -- Eul scepter
+    "item_staff_of_wizardry",
+    "item_void_stone",
+    "item_wind_lace",
+    "item_recipe_cyclone"
 };
 
 function ItemPurchaseThink()
