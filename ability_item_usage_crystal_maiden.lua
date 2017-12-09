2dfda5600356150a9c3588c808ae7324817f6eb8
Ilya Shpigor <petrsum@gmail.com>
2017-12-09 21:15:39 +0100

    Initial add the CM ability usage algorithm

---
+++ ability_item_usage_crystal_maiden.lua
@@ -0,0 +1,20 @@
+local utility = require(
+    GetScriptDirectory() .."/utility/ability_item_usage_utility")
+
+local Abilities = {
+  "shadow_shaman_ether_shock",
+  "shadow_shaman_voodoo",
+  "shadow_shaman_shackles",
+  "shadow_shaman_mass_serpent_ward",
+}
+
+function AbilityUsageThink()
+  local npcBot = GetBot()
+
+  utility.UseGlyph(npcBot)
+
+  utility.UseWard(npcBot, "shadow_shaman_mass_serpent_ward")
+  utility.UseChanneledSingleDisable(npcBot, "shadow_shaman_shackles")
+  utility.UseSingleDisable(npcBot, "shadow_shaman_voodoo")
+  utility.UseMultiNuke(npcBot, "shadow_shaman_ether_shock")
+end
