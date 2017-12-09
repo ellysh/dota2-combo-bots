c018ad590a2e78f1de9a76dcf30bb4babfd00ea6
Ilya Shpigor <petrsum@gmail.com>
2017-12-09 08:46:06 +0100

    Always buy the TP Scroll

--- utility/item_purchase_utility.lua
+++ utility/item_purchase_utility.lua
@@ -1,22 +1,33 @@
 local M = {}
 
+local function IsTpScrollPresent(npcBot)
+  local tpScroll = npcBot:FindItemSlot("item_tpscroll");
+
+  return tpScroll ~= nil;
+end
+
+local function PurchaseTpScroll(npcBot)
+  if IsTpScrollPresent(npcBot) then return end
+
+  if (npcBot:GetGold() >= GetItemCost("item_tpscroll")) then
+    npcBot:ActionImmediate_PurchaseItem("item_tpscroll");
+  end
+end
+
 function M.PurchaseItem(itemsToBuy)
   local npcBot = GetBot();
 
-  if ( #itemsToBuy == 0 )
-  then
-    npcBot:SetNextItemPurchaseValue( 0 );
+  PurchaseTpScroll(npcBot)
+
+  if (#itemsToBuy == 0) then
     return;
   end
 
-  local sNextItem = itemsToBuy[1];
+  local nextItem = itemsToBuy[1];
 
-  npcBot:SetNextItemPurchaseValue( GetItemCost( sNextItem ) );
-
-  if ( npcBot:GetGold() >= GetItemCost( sNextItem ) )
-  then
-    npcBot:ActionImmediate_PurchaseItem( sNextItem );
-    table.remove( itemsToBuy, 1 );
+  if (npcBot:GetGold() >= GetItemCost(nextItem)) then
+      npcBot:ActionImmediate_PurchaseItem(nextItem);
+      table.remove(itemsToBuy, 1);
   end
 end
 
