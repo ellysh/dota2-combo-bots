a5b21c035e66eb89c55c874966007e3201cf30b7
Ilya Shpigor <petrsum@gmail.com>
2017-12-20 22:09:52 +0100

    Add templates for the ITEM_BUILD table

--- generator/launch.sh
+++ generator/launch.sh
@@ -5,3 +5,4 @@ RESULT_DIR="../bot/database"
 
 ./generator.py HEROES 9 $CSV_DIR/heroes.csv > $RESULT_DIR/heroes.lua
 ./generator.py ITEM_RECIPE 5 $CSV_DIR/item_recipe.csv > $RESULT_DIR/item_recipe.lua
+./generator.py ITEM_BUILD 10 $CSV_DIR/item_build.csv > $RESULT_DIR/item_build.lua
