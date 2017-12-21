c4c9c9e6cb47826572019b73f35ea0e64b8371b1
Ilya Shpigor <petrsum@gmail.com>
2017-12-21 17:59:15 +0100

    Implement an algorithm of buying courier

--- bot/item_purchase_crystal_maiden.lua
+++ bot/item_purchase_crystal_maiden.lua
@@ -1,49 +1,6 @@
 local item_purchase_utility = require(
     GetScriptDirectory() .."/utility/item_purchase_utility")
 
-local itemsToBuy = {
-    -- Starting items
-    "item_tango",
-    "item_clarity",
-    "item_branches",
-    "item_branches",
-
-    -- Core items
-    -- Tranquil boots
-    "item_wind_lace",
-    "item_boots",
-    "item_ring_of_regen",
-
-    -- Magic wand
-    "item_magic_stick",
-    "item_enchanted_mango",
-
-    -- Glimmer cape
-    "item_cloak",
-    "item_shadow_amulet",
-
-    -- Extension Items
-    -- Force Stuff
-    "item_staff_of_wizardry",
-    "item_ring_of_health",
-    "item_recipe_force_staff",
-
-    -- Blink Dagger
-    "item_blink",
-
-    -- Lotus Orb
-    --"item_ring_of_health",
-    --"item_void_stone",
-    --"item_platemail",
-    --"item_energy_booster"
-
-    -- Aghanim Scepter
-    "item_point_booster",
-    "item_staff_of_wizardry",
-    "item_ogre_axe",
-    "item_blade_of_alacrity"
-}
-
 function ItemPurchaseThink()
-    item_purchase_utility.PurchaseItem(itemsToBuy)
+    item_purchase_utility.PurchaseItem()
 end
--- bot/utility/item_purchase_utility.lua
+++ bot/utility/item_purchase_utility.lua
@@ -1,12 +1,32 @@
 local logger = require(
     GetScriptDirectory() .."/utility/logger")
 
+local heroes = require(
+    GetScriptDirectory() .."/database/item_recipe")
+
+local heroes = require(
+    GetScriptDirectory() .."/database/item_build")
+
 local M = {}
 
 local function IsTpScrollPresent(npcBot)
   local tpScroll = npcBot:FindItemSlot("item_tpscroll")
 
   return tpScroll ~= -1
+end
+
+local function PurchaseCourier(npcBot)
+  if GetNumCouriers() > 0 then return end
+
+  local players = GetTeamPlayers(GetTeam())
+
+  -- Buy courier only by player of 5th position
+  if players[5] == npcBot:GetPlayerID() then
+
+    logger.Print("PurchaseCourier() - " .. npcBot:GetUnitName() .. " bought Courier")
+
+    npcBot:ActionImmediate_PurchaseItem("item_courier")
+  end
 end
 
 local function PurchaseTpScroll(npcBot)
@@ -20,11 +40,13 @@ local function PurchaseTpScroll(npcBot)
   end
 end
 
-function M.PurchaseItem(itemsToBuy)
+function M.PurchaseItem()
   local npcBot = GetBot()
 
-  PurchaseTpScroll(npcBot)
+  PurchaseCourier(npcBot)
 
+  PurchaseTpScroll(npcBot)
+--[[
   if (#itemsToBuy == 0) then
     return
   end
@@ -36,6 +58,7 @@ function M.PurchaseItem(itemsToBuy)
     npcBot:ActionImmediate_PurchaseItem(nextItem)
     table.remove(itemsToBuy, 1)
   end
+--]]
 end
 
 return M
