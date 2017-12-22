f8dd83d2d8846f629ddae87fd53a1e8c9f511e9b
Ilya Shpigor <petrsum@gmail.com>
2017-12-22 21:34:23 +0100

    Add test for the IsItemSlotsFull function

--- bot/tests/functions_test.lua
+++ bot/tests/functions_test.lua
@@ -56,4 +56,20 @@ function test_GetItemSlotsCount()
   luaunit.assertEquals(functions.test_GetItemSlotsCount(GetBot()), 3)
 end
 
+function test_IsItemSlotsFull()
+  test_RefreshBot()
+
+  luaunit.assertFalse(functions.IsItemSlotsFull(GetBot()))
+
+  table.insert(GetBot().inventory, "item_tango")
+
+  luaunit.assertFalse(functions.IsItemSlotsFull(GetBot()))
+
+  for i = 0, 8, 1 do
+    table.insert(GetBot().inventory, "item_tango")
+  end
+
+  luaunit.assertTrue(functions.IsItemSlotsFull(GetBot()))
+end
+
 os.exit(luaunit.LuaUnit.run())
