cdcb1a6d4c456a7be0e98a661d4b9438cf1498d7
Ilya Shpigor <petrsum@gmail.com>
2017-12-23 17:10:13 +0100

    Remove the deprecated files

--- bot/tests/run_tests.sh
+++ bot/tests/run_tests.sh
@@ -1,18 +1,9 @@
-#!/bin/bash -x
+#!/bin/bash
 
 FILES=$(ls *.lua)
 
-lua hero_selection_test.lua
-
-lua item_purchase_test.lua
-
-lua functions_test.lua
-
-lua courier_test.lua
-
-exit 0
-
 for FILE in $FILES
 do
-    lua $FILE && echo "$FILE: Succeed" || echo "$FILE: Failed"
+  echo "Run $FILE:"
+  lua $FILE
 done
