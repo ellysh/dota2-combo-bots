764beb88dc531101130029dd62a3102e256fcde7
Ilya Shpigor <petrsum@gmail.com>
2017-12-25 17:20:40 +0100

    Add a file to test the level up algorithm in the game

---
+++ bot/ability_item_usage_crystal_maiden.lua
@@ -0,0 +1,6 @@
+local ability_levelup = require(
+    GetScriptDirectory() .."/utility/ability_levelup")
+
+function AbilityLevelUpThink()
+  ability_levelup.AbilityLevelUpThink()
+end
--- bot/item_purchase_crystal_maiden.lua
+++ bot/item_purchase_crystal_maiden.lua
@@ -1,3 +1,6 @@
+-- TODO: Move this code to the item_purchase_generic.lua script.
+-- Remove this file.
+
 local item_purchase = require(
   GetScriptDirectory() .."/utility/item_purchase")
 
