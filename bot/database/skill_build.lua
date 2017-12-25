adde1b7dfe9115a0e11d15278b8fbb2e11c5928d
Ilya Shpigor <petrsum@gmail.com>
2017-12-25 15:37:11 +0100

    Change a format of the SKILL_BUILD table

---
+++ bot/database/skill_build.lua
@@ -0,0 +1,632 @@
+
+local M = {}
+
+M.SKILL_BUILD = {
+
+  npc_dota_hero_crystal_maiden = {
+    skills = {
+      [1] = 2,
+      [2] = 3,
+      [3] = 3,
+      [4] = 1,
+      [5] = 3,
+      [6] = 4,
+      [7] = 3,
+      [8] = 1,
+      [9] = 1,
+      [11] = 1,
+      [12] = 4,
+      [13] = 2,
+      [14] = 2,
+      [16] = 2,
+      [18] = 4
+    },
+    talents = {
+      [10] = 2,
+      [15] = 3,
+      [20] = 6,
+      [25] = 8
+    }
+  },
+
+  npc_dota_hero_juggernaut = {
+    skills = {
+      [1] = nil,
+      [2] = nil,
+      [3] = nil,
+      [4] = nil,
+      [5] = nil,
+      [6] = nil,
+      [7] = nil,
+      [8] = nil,
+      [9] = nil,
+      [11] = nil,
+      [12] = nil,
+      [13] = nil,
+      [14] = nil,
+      [16] = nil,
+      [18] = nil
+    },
+    talents = {
+      [10] = nil,
+      [15] = nil,
+      [20] = nil,
+      [25] = nil
+    }
+  },
+
+  npc_dota_hero_sven = {
+    skills = {
+      [1] = nil,
+      [2] = nil,
+      [3] = nil,
+      [4] = nil,
+      [5] = nil,
+      [6] = nil,
+      [7] = nil,
+      [8] = nil,
+      [9] = nil,
+      [11] = nil,
+      [12] = nil,
+      [13] = nil,
+      [14] = nil,
+      [16] = nil,
+      [18] = nil
+    },
+    talents = {
+      [10] = nil,
+      [15] = nil,
+      [20] = nil,
+      [25] = nil
+    }
+  },
+
+  npc_dota_hero_ursa = {
+    skills = {
+      [1] = nil,
+      [2] = nil,
+      [3] = nil,
+      [4] = nil,
+      [5] = nil,
+      [6] = nil,
+      [7] = nil,
+      [8] = nil,
+      [9] = nil,
+      [11] = nil,
+      [12] = nil,
+      [13] = nil,
+      [14] = nil,
+      [16] = nil,
+      [18] = nil
+    },
+    talents = {
+      [10] = nil,
+      [15] = nil,
+      [20] = nil,
+      [25] = nil
+    }
+  },
+
+  npc_dota_hero_shadow_shaman = {
+    skills = {
+      [1] = nil,
+      [2] = nil,
+      [3] = nil,
+      [4] = nil,
+      [5] = nil,
+      [6] = nil,
+      [7] = nil,
+      [8] = nil,
+      [9] = nil,
+      [11] = nil,
+      [12] = nil,
+      [13] = nil,
+      [14] = nil,
+      [16] = nil,
+      [18] = nil
+    },
+    talents = {
+      [10] = nil,
+      [15] = nil,
+      [20] = nil,
+      [25] = nil
+    }
+  },
+
+  npc_dota_hero_venomancer = {
+    skills = {
+      [1] = nil,
+      [2] = nil,
+      [3] = nil,
+      [4] = nil,
+      [5] = nil,
+      [6] = nil,
+      [7] = nil,
+      [8] = nil,
+      [9] = nil,
+      [11] = nil,
+      [12] = nil,
+      [13] = nil,
+      [14] = nil,
+      [16] = nil,
+      [18] = nil
+    },
+    talents = {
+      [10] = nil,
+      [15] = nil,
+      [20] = nil,
+      [25] = nil
+    }
+  },
+
+  npc_dota_hero_obsidian_destroyer = {
+    skills = {
+      [1] = nil,
+      [2] = nil,
+      [3] = nil,
+      [4] = nil,
+      [5] = nil,
+      [6] = nil,
+      [7] = nil,
+      [8] = nil,
+      [9] = nil,
+      [11] = nil,
+      [12] = nil,
+      [13] = nil,
+      [14] = nil,
+      [16] = nil,
+      [18] = nil
+    },
+    talents = {
+      [10] = nil,
+      [15] = nil,
+      [20] = nil,
+      [25] = nil
+    }
+  },
+
+  npc_dota_hero_death_prophet = {
+    skills = {
+      [1] = nil,
+      [2] = nil,
+      [3] = nil,
+      [4] = nil,
+      [5] = nil,
+      [6] = nil,
+      [7] = nil,
+      [8] = nil,
+      [9] = nil,
+      [11] = nil,
+      [12] = nil,
+      [13] = nil,
+      [14] = nil,
+      [16] = nil,
+      [18] = nil
+    },
+    talents = {
+      [10] = nil,
+      [15] = nil,
+      [20] = nil,
+      [25] = nil
+    }
+  },
+
+  npc_dota_hero_queenofpain = {
+    skills = {
+      [1] = nil,
+      [2] = nil,
+      [3] = nil,
+      [4] = nil,
+      [5] = nil,
+      [6] = nil,
+      [7] = nil,
+      [8] = nil,
+      [9] = nil,
+      [11] = nil,
+      [12] = nil,
+      [13] = nil,
+      [14] = nil,
+      [16] = nil,
+      [18] = nil
+    },
+    talents = {
+      [10] = nil,
+      [15] = nil,
+      [20] = nil,
+      [25] = nil
+    }
+  },
+
+  npc_dota_hero_bristleback = {
+    skills = {
+      [1] = nil,
+      [2] = nil,
+      [3] = nil,
+      [4] = nil,
+      [5] = nil,
+      [6] = nil,
+      [7] = nil,
+      [8] = nil,
+      [9] = nil,
+      [11] = nil,
+      [12] = nil,
+      [13] = nil,
+      [14] = nil,
+      [16] = nil,
+      [18] = nil
+    },
+    talents = {
+      [10] = nil,
+      [15] = nil,
+      [20] = nil,
+      [25] = nil
+    }
+  },
+
+  npc_dota_hero_tidehunter = {
+    skills = {
+      [1] = nil,
+      [2] = nil,
+      [3] = nil,
+      [4] = nil,
+      [5] = nil,
+      [6] = nil,
+      [7] = nil,
+      [8] = nil,
+      [9] = nil,
+      [11] = nil,
+      [12] = nil,
+      [13] = nil,
+      [14] = nil,
+      [16] = nil,
+      [18] = nil
+    },
+    talents = {
+      [10] = nil,
+      [15] = nil,
+      [20] = nil,
+      [25] = nil
+    }
+  },
+
+  npc_dota_hero_shredder = {
+    skills = {
+      [1] = nil,
+      [2] = nil,
+      [3] = nil,
+      [4] = nil,
+      [5] = nil,
+      [6] = nil,
+      [7] = nil,
+      [8] = nil,
+      [9] = nil,
+      [11] = nil,
+      [12] = nil,
+      [13] = nil,
+      [14] = nil,
+      [16] = nil,
+      [18] = nil
+    },
+    talents = {
+      [10] = nil,
+      [15] = nil,
+      [20] = nil,
+      [25] = nil
+    }
+  },
+
+  npc_dota_hero_skywrath_mage = {
+    skills = {
+      [1] = nil,
+      [2] = nil,
+      [3] = nil,
+      [4] = nil,
+      [5] = nil,
+      [6] = nil,
+      [7] = nil,
+      [8] = nil,
+      [9] = nil,
+      [11] = nil,
+      [12] = nil,
+      [13] = nil,
+      [14] = nil,
+      [16] = nil,
+      [18] = nil
+    },
+    talents = {
+      [10] = nil,
+      [15] = nil,
+      [20] = nil,
+      [25] = nil
+    }
+  },
+
+  npc_dota_hero_lion = {
+    skills = {
+      [1] = nil,
+      [2] = nil,
+      [3] = nil,
+      [4] = nil,
+      [5] = nil,
+      [6] = nil,
+      [7] = nil,
+      [8] = nil,
+      [9] = nil,
+      [11] = nil,
+      [12] = nil,
+      [13] = nil,
+      [14] = nil,
+      [16] = nil,
+      [18] = nil
+    },
+    talents = {
+      [10] = nil,
+      [15] = nil,
+      [20] = nil,
+      [25] = nil
+    }
+  },
+
+  npc_dota_hero_sand_king = {
+    skills = {
+      [1] = nil,
+      [2] = nil,
+      [3] = nil,
+      [4] = nil,
+      [5] = nil,
+      [6] = nil,
+      [7] = nil,
+      [8] = nil,
+      [9] = nil,
+      [11] = nil,
+      [12] = nil,
+      [13] = nil,
+      [14] = nil,
+      [16] = nil,
+      [18] = nil
+    },
+    talents = {
+      [10] = nil,
+      [15] = nil,
+      [20] = nil,
+      [25] = nil
+    }
+  },
+
+  npc_dota_hero_lich = {
+    skills = {
+      [1] = nil,
+      [2] = nil,
+      [3] = nil,
+      [4] = nil,
+      [5] = nil,
+      [6] = nil,
+      [7] = nil,
+      [8] = nil,
+      [9] = nil,
+      [11] = nil,
+      [12] = nil,
+      [13] = nil,
+      [14] = nil,
+      [16] = nil,
+      [18] = nil
+    },
+    talents = {
+      [10] = nil,
+      [15] = nil,
+      [20] = nil,
+      [25] = nil
+    }
+  },
+
+  npc_dota_hero_faceless_void = {
+    skills = {
+      [1] = nil,
+      [2] = nil,
+      [3] = nil,
+      [4] = nil,
+      [5] = nil,
+      [6] = nil,
+      [7] = nil,
+      [8] = nil,
+      [9] = nil,
+      [11] = nil,
+      [12] = nil,
+      [13] = nil,
+      [14] = nil,
+      [16] = nil,
+      [18] = nil
+    },
+    talents = {
+      [10] = nil,
+      [15] = nil,
+      [20] = nil,
+      [25] = nil
+    }
+  },
+
+  npc_dota_hero_nevermore = {
+    skills = {
+      [1] = nil,
+      [2] = nil,
+      [3] = nil,
+      [4] = nil,
+      [5] = nil,
+      [6] = nil,
+      [7] = nil,
+      [8] = nil,
+      [9] = nil,
+      [11] = nil,
+      [12] = nil,
+      [13] = nil,
+      [14] = nil,
+      [16] = nil,
+      [18] = nil
+    },
+    talents = {
+      [10] = nil,
+      [15] = nil,
+      [20] = nil,
+      [25] = nil
+    }
+  },
+
+  npc_dota_hero_tiny = {
+    skills = {
+      [1] = nil,
+      [2] = nil,
+      [3] = nil,
+      [4] = nil,
+      [5] = nil,
+      [6] = nil,
+      [7] = nil,
+      [8] = nil,
+      [9] = nil,
+      [11] = nil,
+      [12] = nil,
+      [13] = nil,
+      [14] = nil,
+      [16] = nil,
+      [18] = nil
+    },
+    talents = {
+      [10] = nil,
+      [15] = nil,
+      [20] = nil,
+      [25] = nil
+    }
+  },
+
+  npc_dota_hero_dazzle = {
+    skills = {
+      [1] = nil,
+      [2] = nil,
+      [3] = nil,
+      [4] = nil,
+      [5] = nil,
+      [6] = nil,
+      [7] = nil,
+      [8] = nil,
+      [9] = nil,
+      [11] = nil,
+      [12] = nil,
+      [13] = nil,
+      [14] = nil,
+      [16] = nil,
+      [18] = nil
+    },
+    talents = {
+      [10] = nil,
+      [15] = nil,
+      [20] = nil,
+      [25] = nil
+    }
+  },
+
+  npc_dota_hero_lina = {
+    skills = {
+      [1] = nil,
+      [2] = nil,
+      [3] = nil,
+      [4] = nil,
+      [5] = nil,
+      [6] = nil,
+      [7] = nil,
+      [8] = nil,
+      [9] = nil,
+      [11] = nil,
+      [12] = nil,
+      [13] = nil,
+      [14] = nil,
+      [16] = nil,
+      [18] = nil
+    },
+    talents = {
+      [10] = nil,
+      [15] = nil,
+      [20] = nil,
+      [25] = nil
+    }
+  },
+
+  npc_dota_hero_legion_commander = {
+    skills = {
+      [1] = nil,
+      [2] = nil,
+      [3] = nil,
+      [4] = nil,
+      [5] = nil,
+      [6] = nil,
+      [7] = nil,
+      [8] = nil,
+      [9] = nil,
+      [11] = nil,
+      [12] = nil,
+      [13] = nil,
+      [14] = nil,
+      [16] = nil,
+      [18] = nil
+    },
+    talents = {
+      [10] = nil,
+      [15] = nil,
+      [20] = nil,
+      [25] = nil
+    }
+  },
+
+  npc_dota_hero_drow_ranger = {
+    skills = {
+      [1] = nil,
+      [2] = nil,
+      [3] = nil,
+      [4] = nil,
+      [5] = nil,
+      [6] = nil,
+      [7] = nil,
+      [8] = nil,
+      [9] = nil,
+      [11] = nil,
+      [12] = nil,
+      [13] = nil,
+      [14] = nil,
+      [16] = nil,
+      [18] = nil
+    },
+    talents = {
+      [10] = nil,
+      [15] = nil,
+      [20] = nil,
+      [25] = nil
+    }
+  },
+
+  npc_dota_hero_razor = {
+    skills = {
+      [1] = nil,
+      [2] = nil,
+      [3] = nil,
+      [4] = nil,
+      [5] = nil,
+      [6] = nil,
+      [7] = nil,
+      [8] = nil,
+      [9] = nil,
+      [11] = nil,
+      [12] = nil,
+      [13] = nil,
+      [14] = nil,
+      [16] = nil,
+      [18] = nil
+    },
+    talents = {
+      [10] = nil,
+      [15] = nil,
+      [20] = nil,
+      [25] = nil
+    }
+  },
+
+}
+
+return M
--- generator/launch.sh
+++ generator/launch.sh
@@ -4,6 +4,11 @@ CSV_DIR="database/csv"
 RESULT_DIR="../bot/database"
 
 ./generator.py HEROES 9 $CSV_DIR/heroes.csv > $RESULT_DIR/heroes.lua
+
 ./generator.py ITEM_RECIPE 5 $CSV_DIR/item_recipe.csv > $RESULT_DIR/item_recipe.lua
+
 ./generator.py ITEM_BUILD 11 $CSV_DIR/item_build.csv > $RESULT_DIR/item_build.lua
+
 ./generator.py ITEM_SELL 3 $CSV_DIR/item_sell.csv > $RESULT_DIR/item_sell.lua
+
+./generator.py SKILL_BUILD 20 $CSV_DIR/skill_build.csv > $RESULT_DIR/skill_build.lua
