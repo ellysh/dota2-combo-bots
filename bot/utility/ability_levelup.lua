afdf6fc908c9bee8d4f0d141927eb3a3fd904817
Ilya Shpigor <petrsum@gmail.com>
2017-12-25 16:59:58 +0100

    Implement the InitAbilities function

--- bot/utility/ability_levelup.lua
+++ bot/utility/ability_levelup.lua
@@ -6,4 +6,29 @@ local skill_build = require(
 
 local M = {}
 
+ABILITIES = {}
+
+TALENTS = {}
+
+local function InitAbilities()
+  local npc_bot = GetBot()
+
+  for i = 0, 23, 1 do
+
+    local ability = npc_bot:GetAbilityInSlot(i)
+
+    if ability ~= nil and ability:GetName() ~= "generic_hidden" then
+      if ability:IsTalent() then
+        table.insert(TALENTS, ability)
+      else
+        table.insert(ABILITIES, ability)
+      end
+    end
+  end
+end
+
+function M.AbilityLevelUpThink()
+  -- TODO: Implement this function
+end
+
 return M
