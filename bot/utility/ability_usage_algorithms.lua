d01734d8d8c4c8e12788fc68ce20e7a5d5693521
Ilya Shpigor <petrsum@gmail.com>
2017-12-30 19:51:25 +0100

    Stub implementation of the low_hp_enemy_hero_to_kill algorithm

--- bot/utility/ability_usage_algorithms.lua
+++ bot/utility/ability_usage_algorithms.lua
@@ -3,9 +3,37 @@ local logger = require(
 
 local M = {}
 
-function M.low_hp_enemy_hero_to_kill()
+local function GetEnemyHeroMinHp(radius)
   -- TODO: Implement this function
-  return BOT_ACTION_DESIRE_HIGH, {0, 0}
+  return nil
+end
+
+local function IsTargetable(npc)
+  -- TODO: Implement this function
+  return false
+end
+
+local function IsEnoughDamageToKill(npc, ability)
+  -- TODO: Implement this function
+  return false
+end
+
+local function GetTarget(target, ability)
+  -- TODO: Implement this function
+  return nil
+end
+
+function M.low_hp_enemy_hero_to_kill(ability)
+  local enemy_hero = GetEnemyHeroMinHp(ability:GetCastRange())
+
+  if enemy_hero == nil
+    or not IsTargetable(enemy_hero)
+    or not IsEnoughDamageToKill(enemy_hero, ability) then
+
+    return BOT_ACTION_DESIRE_NONE, nil
+  end
+
+  return BOT_ACTION_DESIRE_HIGH, GetTarget(enemy_hero, ability)
 end
 
 return M
