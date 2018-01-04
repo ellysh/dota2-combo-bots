cdc02d392c90973b5257467edea94ead16a9ea3e
Ilya Shpigor <petrsum@gmail.com>
2018-01-04 15:47:36 +0100

    Change the max_networth_enemy_hero algorithm to the max_kills_enemy_hero in the Database

--- bot/database/skill_usage.lua
+++ bot/database/skill_usage.lua
@@ -9,29 +9,29 @@ M.SKILL_USAGE = {
   crystal_maiden_crystal_nova = {
       any_mode = algorithms["min_hp_enemy_hero_to_kill"],
       team_fight = algorithms["three_and_more_enemy_heroes"],
-      BOT_MODE_ROAM = algorithms["max_networth_enemy_hero"],
-      BOT_MODE_TEAM_ROAM = algorithms["max_networth_enemy_hero"],
+      BOT_MODE_ROAM = algorithms["max_kills_enemy_hero"],
+      BOT_MODE_TEAM_ROAM = algorithms["max_kills_enemy_hero"],
       BOT_MODE_PUSH_TOWER = algorithms["three_and_more_creeps"],
-      BOT_MODE_ATTACK = algorithms["max_networth_enemy_hero"],
+      BOT_MODE_ATTACK = algorithms["max_kills_enemy_hero"],
       BOT_MODE_LANING = algorithms["three_and_more_creeps"],
       BOT_MODE_FARM = algorithms["three_and_more_creeps"],
       BOT_MODE_DEFEND_TOWER = algorithms["three_and_more_creeps"],
       BOT_MODE_RETREAT = algorithms["last_attacked_enemy_hero"],
-      BOT_MODE_DEFEND_ALLY = algorithms["max_networth_enemy_hero"]
+      BOT_MODE_DEFEND_ALLY = algorithms["max_kills_enemy_hero"]
   },
 
   crystal_maiden_frostbite = {
       any_mode = algorithms["channeling_enemy_hero"],
-      team_fight = algorithms["max_networth_enemy_hero"],
-      BOT_MODE_ROAM = algorithms["max_networth_enemy_hero"],
-      BOT_MODE_TEAM_ROAM = algorithms["max_networth_enemy_hero"],
-      BOT_MODE_PUSH_TOWER = algorithms["max_networth_enemy_hero"],
-      BOT_MODE_ATTACK = algorithms["max_networth_enemy_hero"],
-      BOT_MODE_LANING = algorithms["max_networth_enemy_hero"],
+      team_fight = algorithms["max_kills_enemy_hero"],
+      BOT_MODE_ROAM = algorithms["max_kills_enemy_hero"],
+      BOT_MODE_TEAM_ROAM = algorithms["max_kills_enemy_hero"],
+      BOT_MODE_PUSH_TOWER = algorithms["max_kills_enemy_hero"],
+      BOT_MODE_ATTACK = algorithms["max_kills_enemy_hero"],
+      BOT_MODE_LANING = algorithms["max_kills_enemy_hero"],
       BOT_MODE_FARM = algorithms["max_hp_creep"],
-      BOT_MODE_DEFEND_TOWER = algorithms["max_networth_enemy_hero"],
+      BOT_MODE_DEFEND_TOWER = algorithms["max_kills_enemy_hero"],
       BOT_MODE_RETREAT = algorithms["last_attacked_enemy_hero"],
-      BOT_MODE_DEFEND_ALLY = algorithms["max_networth_enemy_hero"]
+      BOT_MODE_DEFEND_ALLY = algorithms["max_kills_enemy_hero"]
   },
 
   crystal_maiden_freezing_field = {
@@ -120,30 +120,30 @@ M.SKILL_USAGE = {
 
   juggernaut_omni_slash = {
       any_mode = algorithms["min_hp_enemy_hero_to_kill"],
-      team_fight = algorithms["max_networth_enemy_hero"],
-      BOT_MODE_ROAM = algorithms["max_networth_enemy_hero"],
-      BOT_MODE_TEAM_ROAM = algorithms["max_networth_enemy_hero"],
-      BOT_MODE_PUSH_TOWER = algorithms["max_networth_enemy_hero"],
-      BOT_MODE_ATTACK = algorithms["max_networth_enemy_hero"],
+      team_fight = algorithms["max_kills_enemy_hero"],
+      BOT_MODE_ROAM = algorithms["max_kills_enemy_hero"],
+      BOT_MODE_TEAM_ROAM = algorithms["max_kills_enemy_hero"],
+      BOT_MODE_PUSH_TOWER = algorithms["max_kills_enemy_hero"],
+      BOT_MODE_ATTACK = algorithms["max_kills_enemy_hero"],
       BOT_MODE_LANING = algorithms["nil"],
       BOT_MODE_FARM = algorithms["nil"],
-      BOT_MODE_DEFEND_TOWER = algorithms["max_networth_enemy_hero"],
+      BOT_MODE_DEFEND_TOWER = algorithms["max_kills_enemy_hero"],
       BOT_MODE_RETREAT = algorithms["nil"],
-      BOT_MODE_DEFEND_ALLY = algorithms["max_networth_enemy_hero"]
+      BOT_MODE_DEFEND_ALLY = algorithms["max_kills_enemy_hero"]
   },
 
   shadow_shaman_ether_shock = {
       any_mode = algorithms["min_hp_enemy_hero_to_kill"],
-      team_fight = algorithms["max_networth_enemy_hero"],
-      BOT_MODE_ROAM = algorithms["max_networth_enemy_hero"],
-      BOT_MODE_TEAM_ROAM = algorithms["max_networth_enemy_hero"],
-      BOT_MODE_PUSH_TOWER = algorithms["max_networth_enemy_hero"],
-      BOT_MODE_ATTACK = algorithms["max_networth_enemy_hero"],
+      team_fight = algorithms["max_kills_enemy_hero"],
+      BOT_MODE_ROAM = algorithms["max_kills_enemy_hero"],
+      BOT_MODE_TEAM_ROAM = algorithms["max_kills_enemy_hero"],
+      BOT_MODE_PUSH_TOWER = algorithms["max_kills_enemy_hero"],
+      BOT_MODE_ATTACK = algorithms["max_kills_enemy_hero"],
       BOT_MODE_LANING = algorithms["nil"],
       BOT_MODE_FARM = algorithms["nil"],
-      BOT_MODE_DEFEND_TOWER = algorithms["max_networth_enemy_hero"],
+      BOT_MODE_DEFEND_TOWER = algorithms["max_kills_enemy_hero"],
       BOT_MODE_RETREAT = algorithms["nil"],
-      BOT_MODE_DEFEND_ALLY = algorithms["max_networth_enemy_hero"]
+      BOT_MODE_DEFEND_ALLY = algorithms["max_kills_enemy_hero"]
   },
 
   shadow_shaman_voodoo = {
