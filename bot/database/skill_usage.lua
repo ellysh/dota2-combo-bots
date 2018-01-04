4759eb994cc30729d70af946c262f39fdfc317c1
Ilya Shpigor <petrsum@gmail.com>
2018-01-04 12:13:29 +0100

    Complete skill usage description for Shadow Shaman

--- bot/database/skill_usage.lua
+++ bot/database/skill_usage.lua
@@ -64,15 +64,15 @@ M.SKILL_USAGE = {
 
   drow_ranger_wave_of_silence = {
       any_mode = algorithms["nil"],
-      team_fight = algorithms["nil"],
+      team_fight = algorithms["max_offensive_power_enemy_hero"],
       BOT_MODE_ROAM = algorithms["max_offensive_power_enemy_hero"],
       BOT_MODE_TEAM_ROAM = algorithms["max_offensive_power_enemy_hero"],
-      BOT_MODE_PUSH_TOWER = algorithms["max_offensive_power_enemy_hero"],
+      BOT_MODE_PUSH_TOWER = algorithms["nil"],
       BOT_MODE_ATTACK = algorithms["max_offensive_power_enemy_hero"],
       BOT_MODE_LANING = algorithms["nil"],
       BOT_MODE_FARM = algorithms["nil"],
       BOT_MODE_DEFEND_TOWER = algorithms["max_offensive_power_enemy_hero"],
-      BOT_MODE_RETREAT = algorithms["nil"],
+      BOT_MODE_RETREAT = algorithms["last_attacked_enemy_hero"],
       BOT_MODE_DEFEND_ALLY = algorithms["max_offensive_power_enemy_hero"]
   },
 
@@ -130,6 +130,62 @@ M.SKILL_USAGE = {
       BOT_MODE_DEFEND_TOWER = algorithms["max_networth_enemy_hero"],
       BOT_MODE_RETREAT = algorithms["nil"],
       BOT_MODE_DEFEND_ALLY = algorithms["max_networth_enemy_hero"]
+  },
+
+  shadow_shaman_ether_shock = {
+      any_mode = algorithms["min_hp_enemy_hero_to_kill"],
+      team_fight = algorithms["max_networth_enemy_hero"],
+      BOT_MODE_ROAM = algorithms["max_networth_enemy_hero"],
+      BOT_MODE_TEAM_ROAM = algorithms["max_networth_enemy_hero"],
+      BOT_MODE_PUSH_TOWER = algorithms["max_networth_enemy_hero"],
+      BOT_MODE_ATTACK = algorithms["max_networth_enemy_hero"],
+      BOT_MODE_LANING = algorithms["nil"],
+      BOT_MODE_FARM = algorithms["nil"],
+      BOT_MODE_DEFEND_TOWER = algorithms["max_networth_enemy_hero"],
+      BOT_MODE_RETREAT = algorithms["nil"],
+      BOT_MODE_DEFEND_ALLY = algorithms["max_networth_enemy_hero"]
+  },
+
+  shadow_shaman_voodoo = {
+      any_mode = algorithms["channeling_enemy_hero"],
+      team_fight = algorithms["max_offensive_power_enemy_hero"],
+      BOT_MODE_ROAM = algorithms["max_offensive_power_enemy_hero"],
+      BOT_MODE_TEAM_ROAM = algorithms["max_offensive_power_enemy_hero"],
+      BOT_MODE_PUSH_TOWER = algorithms["nil"],
+      BOT_MODE_ATTACK = algorithms["max_offensive_power_enemy_hero"],
+      BOT_MODE_LANING = algorithms["nil"],
+      BOT_MODE_FARM = algorithms["nil"],
+      BOT_MODE_DEFEND_TOWER = algorithms["max_offensive_power_enemy_hero"],
+      BOT_MODE_RETREAT = algorithms["last_attacked_enemy_hero"],
+      BOT_MODE_DEFEND_ALLY = algorithms["max_offensive_power_enemy_hero"]
+  },
+
+  shadow_shaman_shackles = {
+      any_mode = algorithms["channeling_enemy_hero"],
+      team_fight = algorithms["max_offensive_power_enemy_hero"],
+      BOT_MODE_ROAM = algorithms["nil"],
+      BOT_MODE_TEAM_ROAM = algorithms["max_offensive_power_enemy_hero"],
+      BOT_MODE_PUSH_TOWER = algorithms["nil"],
+      BOT_MODE_ATTACK = algorithms["nil"],
+      BOT_MODE_LANING = algorithms["nil"],
+      BOT_MODE_FARM = algorithms["nil"],
+      BOT_MODE_DEFEND_TOWER = algorithms["nil"],
+      BOT_MODE_RETREAT = algorithms["nil"],
+      BOT_MODE_DEFEND_ALLY = algorithms["max_offensive_power_enemy_hero"]
+  },
+
+  shadow_shaman_mass_serpent_ward = {
+      any_mode = algorithms["nil"],
+      team_fight = algorithms["three_and_more_enemy_heroes"],
+      BOT_MODE_ROAM = algorithms["nil"],
+      BOT_MODE_TEAM_ROAM = algorithms["nil"],
+      BOT_MODE_PUSH_TOWER = algorithms["min_hp_enemy_building"],
+      BOT_MODE_ATTACK = algorithms["nil"],
+      BOT_MODE_LANING = algorithms["nil"],
+      BOT_MODE_FARM = algorithms["nil"],
+      BOT_MODE_DEFEND_TOWER = algorithms["nil"],
+      BOT_MODE_RETREAT = algorithms["nil"],
+      BOT_MODE_DEFEND_ALLY = algorithms["nil"]
   },
 
 }
