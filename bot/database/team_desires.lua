5787b4bb10c83e8d24f08d96a923099b0fc01f10
Ilya Shpigor <petrsum@gmail.com>
2018-01-07 20:38:01 +0100

    Fix bug with the time_is_less_5_minutes algorithm desires

--- bot/database/team_desires.lua
+++ bot/database/team_desires.lua
@@ -34,9 +34,9 @@ M.TEAM_DESIRES = {
   },
 
   time_is_less_5_minutes = {
-    PUSH_TOP_LINE_DESIRE = {0.2, -0.2},
-    PUSH_MID_LINE_DESIRE = {0.2, -0.2},
-    PUSH_BOT_LINE_DESIRE = {0.2, -0.2}
+    PUSH_TOP_LINE_DESIRE = {-0.2, 0.2},
+    PUSH_MID_LINE_DESIRE = {-0.2, 0.2},
+    PUSH_BOT_LINE_DESIRE = {-0.2, 0.2}
   },
 
   three_and_more_ally_heroes_on_top = {
