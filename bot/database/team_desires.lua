3002849f7ff250d9bc7158eae45950cea68b1add
Ilya Shpigor <petrsum@gmail.com>
2018-01-07 19:34:14 +0100

    Update values of the team desires

--- bot/database/team_desires.lua
+++ bot/database/team_desires.lua
@@ -22,21 +22,21 @@ M.TEAM_DESIRES = {
   },
 
   max_kills_enemy_hero_alive = {
-    PUSH_TOP_LINE_DESIRE = {-0.2, 0.2},
-    PUSH_MID_LINE_DESIRE = {-0.2, 0.2},
+    PUSH_TOP_LINE_DESIRE = {-0.1, 0.2},
+    PUSH_MID_LINE_DESIRE = {-0.1, 0.2},
     PUSH_BOT_LINE_DESIRE = {-0.2, 0.2}
   },
 
   max_kills_ally_hero_alive = {
-    PUSH_TOP_LINE_DESIRE = {0.2, -0.2},
-    PUSH_MID_LINE_DESIRE = {0.2, -0.2},
+    PUSH_TOP_LINE_DESIRE = {0.1, -0.2},
+    PUSH_MID_LINE_DESIRE = {0.1, -0.2},
     PUSH_BOT_LINE_DESIRE = {0.2, -0.2}
   },
 
   time_is_less_5_minutes = {
-    PUSH_TOP_LINE_DESIRE = {0, -0.2},
-    PUSH_MID_LINE_DESIRE = {0, -0.2},
-    PUSH_BOT_LINE_DESIRE = {0, -0.2}
+    PUSH_TOP_LINE_DESIRE = {0.2, -0.2},
+    PUSH_MID_LINE_DESIRE = {0.2, -0.2},
+    PUSH_BOT_LINE_DESIRE = {0.2, -0.2}
   },
 
 }
