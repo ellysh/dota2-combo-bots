3affaadaabda7ed98c286e5cba5913a374a265cb
Ilya Shpigor <petrsum@gmail.com>
2018-01-10 23:01:20 +0100

    Release 0.6

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Dota 2 Combo Bots 0.5 version
+# Dota 2 Combo Bots 0.6 version
 
 *This project is still in prototyping and developing stage.*
 
--- bot/utility/logger.lua
+++ bot/utility/logger.lua
@@ -4,6 +4,6 @@ function M.Print(string)
   print(GameTime() .. ": " ..string .. "\n")
 end
 
-M.Print("Combo Bots version 0.5")
+M.Print("Combo Bots version 0.6")
 
 return M
