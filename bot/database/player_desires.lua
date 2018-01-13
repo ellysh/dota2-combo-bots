b2fe60c39530935dd8724eebec2c551c8c772ce9
Ilya Shpigor <petrsum@gmail.com>
2018-01-13 09:28:06 +0100

    Add new algorithms to the PLAYER_DESIRES table

--- bot/database/player_desires.lua
+++ bot/database/player_desires.lua
@@ -4,9 +4,33 @@ local M = {}
 M.PLAYER_DESIRES = {
 
   have_low_hp = {
-    PUSH_TOP_LINE_DESIRE = {-0.2, 0},
-    PUSH_MID_LINE_DESIRE = {-0.2, 0},
-    PUSH_BOT_LINE_DESIRE = {-0.2, 0}
+    PUSH_TOP_LINE_DESIRE = {-0.1, 0},
+    PUSH_MID_LINE_DESIRE = {-0.1, 0},
+    PUSH_BOT_LINE_DESIRE = {-0.1, 0}
+  },
+
+  player_on_top = {
+    PUSH_TOP_LINE_DESIRE = {0.05, 0},
+    PUSH_MID_LINE_DESIRE = {-0.05, 0},
+    PUSH_BOT_LINE_DESIRE = {-0.05, 0}
+  },
+
+  player_on_mid = {
+    PUSH_TOP_LINE_DESIRE = {-0.05, 0},
+    PUSH_MID_LINE_DESIRE = {0.05, 0},
+    PUSH_BOT_LINE_DESIRE = {-0.05, 0}
+  },
+
+  player_on_bot = {
+    PUSH_TOP_LINE_DESIRE = {-0.05, 0},
+    PUSH_MID_LINE_DESIRE = {-0.05, 0},
+    PUSH_BOT_LINE_DESIRE = {0.05, 0}
+  },
+
+  have_tp_scroll_or_travel_boots = {
+    PUSH_TOP_LINE_DESIRE = {0.05, -0.05},
+    PUSH_MID_LINE_DESIRE = {0.05, -0.05},
+    PUSH_BOT_LINE_DESIRE = {0.05, -0.05}
   },
 
 }
