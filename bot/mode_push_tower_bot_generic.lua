e07cd39fe43a599a4b27e80785ef6271501a777f
Ilya Shpigor <petrsum@gmail.com>
2018-01-13 08:59:20 +0100

    Refactoring of the mode_push_*_generic modules

--- bot/mode_push_tower_bot_generic.lua
+++ bot/mode_push_tower_bot_generic.lua
@@ -1,30 +1,10 @@
-local desires = require(
-  GetScriptDirectory() .."/utility/desires")
-
-local player_desires = require(
-  GetScriptDirectory() .."/database/player_desires")
-
-local player_desires_algorithms = require(
-  GetScriptDirectory() .."/utility/player_desires_algorithms")
-
-local PLAYER_DESIRES = {
-  PUSH_TOP_LINE_DESIRE = 0,
-  PUSH_MID_LINE_DESIRE = 0,
-  PUSH_BOT_LINE_DESIRE = 0
-}
+local mode_push = require(
+  GetScriptDirectory() .."/utility/mode_push")
 
 function GetDesire()
-  PLAYER_DESIRES = desires.Think(
-    player_desires.PLAYER_DESIRES,
-    player_desires_algorithms)
-
-  return GetPushLaneDesire(LANE_BOT) + PLAYER_DESIRES.PUSH_BOT_LINE_DESIRE
+  return mode_push.GetDesire(LANE_BOT)
 end
 
 function Think()
-  -- TODO: Use TP scrolls and TP boots here
-
-  local npc_bot = GetBot()
-  local target = GetLaneFrontLocation(GetTeam(), LANE_BOT, 0.5)
-  npc_bot:Action_AttackMove(target)
+  mode_push.Think(LANE_BOT)
 end
