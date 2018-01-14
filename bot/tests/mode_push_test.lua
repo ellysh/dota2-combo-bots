60d6d5f332a33f649e91a05ed0795b930f141406
Ilya Shpigor <petrsum@gmail.com>
2018-01-14 11:33:36 +0100

    Fix a broken unit tests

--- bot/tests/mode_push_test.lua
+++ bot/tests/mode_push_test.lua
@@ -11,16 +11,25 @@ function test_GetDesire()
   test_RefreshBot()
 
   PUSH_LANE_DESIRE = 0
-  luaunit.assertEquals(mode_push.GetDesire(LANE_TOP), 0)
+  luaunit.assertAlmostEquals(
+    mode_push.GetDesire(LANE_TOP),
+    -0.05,
+    0.001)
 
   PUSH_LANE_DESIRE = 0.5
-  luaunit.assertEquals(mode_push.GetDesire(LANE_MID), 0.5)
+  luaunit.assertAlmostEquals(
+    mode_push.GetDesire(LANE_MID),
+    0.45,
+    0.001)
 
   PUSH_LANE_DESIRE = 0.7
-  luaunit.assertEquals(mode_push.GetDesire(LANE_BOT), 0.7)
+  luaunit.assertAlmostEquals(
+    mode_push.GetDesire(LANE_BOT),
+    0.65,
+    0.001)
 end
 
-function test_GetDesire()
+function test_Think()
   test_RefreshBot()
 
   mode_push.Think(LANE_TOP)
