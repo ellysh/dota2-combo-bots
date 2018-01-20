9e6574b488005fb38b78c831ea5c1d9ac5d6d298
Ilya Shpigor <petrsum@gmail.com>
2018-01-20 21:41:51 +0100

    Remove the PLAYER_DESIRES global variable

--- bots/utility/mode_push.lua
+++ bots/utility/mode_push.lua
@@ -9,12 +9,6 @@ local player_desires_algorithms = require(
 
 local M = {}
 
-local PLAYER_DESIRES = {
-  PUSH_TOP_LINE_DESIRE = 0,
-  PUSH_MID_LINE_DESIRE = 0,
-  PUSH_BOT_LINE_DESIRE = 0
-}
-
 local LANE_TO_DESIRE= {
   [LANE_TOP] = "PUSH_TOP_LINE_DESIRE",
   [LANE_MID] = "PUSH_MID_LINE_DESIRE",
@@ -22,11 +16,11 @@ local LANE_TO_DESIRE= {
 }
 
 function M.GetDesire(lane)
-  PLAYER_DESIRES = desires.Think(
+  local desires_list = desires.Think(
     player_desires.PLAYER_DESIRES,
     player_desires_algorithms)
 
-  return GetPushLaneDesire(lane) + PLAYER_DESIRES[LANE_TO_DESIRE[lane]]
+  return GetPushLaneDesire(lane) + desires_list[LANE_TO_DESIRE[lane]]
 end
 
 function M.Think(lane)
