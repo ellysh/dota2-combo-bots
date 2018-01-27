fc38db6244cf0621feaa4a94631688afa085380c
Ilya Shpigor <petrsum@gmail.com>
2018-01-27 09:30:44 +0100

    Fix a bug with picking up the rune at the beginning of the match

--- bots/mode_rune_generic.lua
+++ bots/mode_rune_generic.lua
@@ -1,3 +1,6 @@
+local functions = require(
+  GetScriptDirectory() .."/utility/functions")
+
 local constants = require(
   GetScriptDirectory() .."/utility/constants")
 
@@ -22,20 +25,44 @@ local function GetClosestRune(bot)
     rune_distance,
     function(t, a, b) return t[a] < t[b] end)
 
-  if constants.MAX_HERO_DISTANCE_FROM_RUNE <= distance then
+  if distance < constants.MAX_HERO_DISTANCE_FROM_RUNE then
     return rune, distance
   else
     return nil, 0
   end
 end
 
+local function IsBeginningOfMatch()
+  return DotaTime() < 0
+end
+
+local function IsRuneAppearSoon()
+  local time = DotaTime()
+
+  if time < 0 then
+    return false end
+
+  local last_appear = time - (time % 2)
+
+  return 1.9 <= (time - last_appear)
+end
+
 function GetDesire()
+  local bot = GetBot()
+
   if functions.IsBotInFightingMode(bot) then
     return 0 end
 
-  local rune, distance = GetClosestRune(GetBot())
+  local rune, distance = GetClosestRune(bot)
 
-  if rune == nil or GetRuneStatus(rune) == RUNE_STATUS_MISSING then
+  if rune == nil then
+    return 0 end
+
+  if IsBeginningOfMatch() then
+    return 0.75 end
+
+  if GetRuneStatus(rune) == RUNE_STATUS_MISSING
+     and not IsRuneAppearSoon() then
     return 0 end
 
   if GetRuneStatus(rune) == RUNE_STATUS_AVAILABLE then
@@ -43,12 +70,19 @@ function GetDesire()
 
   -- TODO: This code is the same as one ine the mode_shop.lua, GetDesire()
   -- Move it to a separate function.
-  return (1 - (distance / constants.MAX_HERO_DISTANCE_FROM_RUNE)) + 0.2
+  return (1 - (distance / constants.MAX_HERO_DISTANCE_FROM_RUNE)) + 0.3
 end
 
 function Think()
   local bot = GetBot()
   local rune, distance = GetClosestRune(bot)
 
-  bot:Action_PickUpRune(rune)
+  if rune == nil then
+    return end
+
+  if bot:GetCurrentVisionRange() < distance then
+    bot:Action_MoveToLocation(GetRuneSpawnLocation(rune))
+  else
+    bot:Action_PickUpRune(rune)
+  end
 end
