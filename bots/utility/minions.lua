262f4e1ff1022e57786a92eed5a4a0306d2d48d0
Ilya Shpigor <petrsum@gmail.com>
2018-01-27 15:49:03 +0100

    Base implementation of the minions logic

--- bots/utility/minions.lua
+++ bots/utility/minions.lua
@@ -1,7 +1,23 @@
+local constants = require(
+  GetScriptDirectory() .."/utility/constants")
+
 local M = {}
 
 function M.MinionThink(minion)
-  -- TODO: Implement this function
+  local bot = GetBot()
+
+  if constants.MAX_MINION_DISTANCE_FROM_HERO
+     < GetUnitToUnitDistance(bot, minion) then
+
+     minion:Action_MoveToLocation(bot:GetExtrapolatedLocation(3.0))
+     return
+   end
+
+   local target = bot:GetTarget()
+
+   if target ~= nil then
+     minion:Action_AttackUnit(target, false)
+   end
 end
 
 return M
