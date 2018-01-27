d9970b0c9fd5bc2709740404e6de2bed7446d8eb
Ilya Shpigor <petrsum@gmail.com>
2018-01-27 19:35:54 +0100

    Fix a bug with the 1st Lich skill usage

--- bots/database/skill_usage.lua
+++ bots/database/skill_usage.lua
@@ -397,18 +397,18 @@ M.SKILL_USAGE = {
   },
 
   lich_frost_nova = {
-      any_mode = {algorithms["min_hp_enemy_hero_to_kill"], 90},
-      team_fight = {algorithms["attacked_enemy_hero"], 90},
-      BOT_MODE_ROAM = {algorithms["attacked_enemy_hero"], 70},
-      BOT_MODE_TEAM_ROAM = {algorithms["attacked_enemy_hero"], 70},
-      BOT_MODE_PUSH_TOWER = {algorithms["three_and_more_creeps"], 90},
-      BOT_MODE_ATTACK = {algorithms["attacked_enemy_hero"], 90},
-      BOT_MODE_LANING = {algorithms["three_and_more_creeps"], 50},
+      any_mode = {algorithms["channeling_enemy_hero"], 90},
+      team_fight = {algorithms["attacked_enemy_hero"], 70},
+      BOT_MODE_ROAM = {algorithms["attacked_enemy_hero"], 90},
+      BOT_MODE_TEAM_ROAM = {algorithms["attacked_enemy_hero"], 90},
+      BOT_MODE_PUSH_TOWER = {algorithms["attacked_enemy_hero"], 70},
+      BOT_MODE_ATTACK = {algorithms["attacked_enemy_hero"], 70},
+      BOT_MODE_LANING = {algorithms["nil"], nil},
       BOT_MODE_ROSHAN = {algorithms["attacked_enemy_creep"], 70},
-      BOT_MODE_FARM = {algorithms["three_and_more_creeps"], 60},
-      BOT_MODE_DEFEND_TOWER = {algorithms["three_and_more_creeps"], 90},
-      BOT_MODE_RETREAT = {algorithms["last_attacked_enemy_hero"], 30},
-      BOT_MODE_DEFEND_ALLY = {algorithms["attacked_enemy_hero"], 70}
+      BOT_MODE_FARM = {algorithms["attacked_enemy_creep"], 60},
+      BOT_MODE_DEFEND_TOWER = {algorithms["attacked_enemy_hero"], 70},
+      BOT_MODE_RETREAT = {algorithms["last_attacked_enemy_hero"], 50},
+      BOT_MODE_DEFEND_ALLY = {algorithms["attacked_enemy_hero"], 80}
   },
 
   lich_frost_armor = {
