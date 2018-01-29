2c8ee6602ad11d706f1cd9920e1a65f57c20dbf1
Ilya Shpigor <petrsum@gmail.com>
2018-01-29 19:45:43 +0100

    Refactoring unit tests for the attack algorithms

--- bots/tests/attack_algorithms_test.lua
+++ bots/tests/attack_algorithms_test.lua
@@ -33,198 +33,128 @@ function test_IsTargetable()
   UNIT_IS_ILLUSION = false
 end
 
-function test_max_kills_enemy_hero_succeed()
+local function test_algorithm_pattern_succeed(algorithm, expect_target)
+
   test_RefreshBot()
 
-  UNIT_CAN_BE_SEEN = true
-
-  local desire, target = algorithms.max_kills_enemy_hero(
+  local desire, target = algorithms[algorithm](
     GetBot(),
     1200)
 
-  luaunit.assertEquals(desire, true)
-  luaunit.assertEquals(target:GetUnitName(), "unit1")
+  luaunit.assertTrue(desire)
+  luaunit.assertEquals(target:GetUnitName(), expect_target)
+end
+
+local function test_algorithm_pattern_fails(algorithm)
+
+  test_RefreshBot()
+
+  local desire, target = algorithms[algorithm](
+    GetBot(),
+    1200)
+
+  luaunit.assertFalse(desire)
+  luaunit.assertEquals(target, nil)
+end
+
+function test_max_kills_enemy_hero_succeed()
+  UNIT_CAN_BE_SEEN = true
+
+  test_algorithm_pattern_succeed("max_kills_enemy_hero", "unit1")
 end
 
 function test_max_kills_enemy_hero_not_targetable_fails()
-  test_RefreshBot()
-
   UNIT_CAN_BE_SEEN = false
 
-  local desire, target = algorithms.max_kills_enemy_hero(
-    GetBot(),
-    1200)
-
-  luaunit.assertEquals(desire, false)
-  luaunit.assertEquals(target, nil)
+  test_algorithm_pattern_fails("max_kills_enemy_hero")
 end
 
 function test_max_estimated_damage_enemy_hero_succeed()
-  test_RefreshBot()
-
   UNIT_CAN_BE_SEEN = true
 
-  local desire, target = algorithms.max_estimated_damage_enemy_hero(
-    GetBot(),
-    1200)
-
-  luaunit.assertEquals(desire, true)
-  luaunit.assertEquals(target:GetUnitName(), "unit2")
+  test_algorithm_pattern_succeed(
+    "max_estimated_damage_enemy_hero",
+    "unit2")
 end
 
 function test_max_estimated_damage_enemy_hero_not_targetable_fails()
-  test_RefreshBot()
-
   UNIT_CAN_BE_SEEN = false
 
-  local desire, target = algorithms.max_estimated_damage_enemy_hero(
-    GetBot(),
-    1200)
-
-  luaunit.assertEquals(desire, false)
-  luaunit.assertEquals(target, nil)
+  test_algorithm_pattern_fails(
+    "max_estimated_damage_enemy_hero",
+    "unit2")
 end
 
 function test_max_hp_creep_succeed()
-  test_RefreshBot()
-
   UNIT_CAN_BE_SEEN = true
   UNIT_NO_NEARBY_UNITS = false
 
-  local desire, target = algorithms.max_hp_creep(
-    GetBot(),
-    1200)
-
-  luaunit.assertEquals(desire, true)
-  luaunit.assertEquals(target:GetUnitName(), "creep2")
+  test_algorithm_pattern_succeed("max_hp_creep", "creep2")
 end
 
 function test_max_hp_creep_no_unit_fails()
-  test_RefreshBot()
-
   UNIT_CAN_BE_SEEN = true
   UNIT_NO_NEARBY_UNITS = true
 
-  local desire, target = algorithms.max_hp_creep(
-    GetBot(),
-    1200)
-
-  luaunit.assertEquals(desire, false)
-  luaunit.assertEquals(target, nil)
+  test_algorithm_pattern_fails("max_hp_creep", "creep2")
 end
 
 function test_last_hit_creep_succeed()
-  test_RefreshBot()
-
   UNIT_CAN_BE_SEEN = true
   UNIT_NO_NEARBY_UNITS = false
 
-  local desire, target = algorithms.last_hit_creep(
-    GetBot(),
-    1200)
-
-  luaunit.assertEquals(desire, true)
-  luaunit.assertEquals(target:GetUnitName(), "creep1")
+  test_algorithm_pattern_succeed("last_hit_creep", "creep1")
 end
 
 function test_last_hit_creep_no_unit_fails()
-  test_RefreshBot()
-
   UNIT_CAN_BE_SEEN = true
   UNIT_NO_NEARBY_UNITS = true
 
-  local desire, target = algorithms.last_hit_creep(
-    GetBot(),
-    1200)
-
-  luaunit.assertEquals(desire, false)
-  luaunit.assertEquals(target, nil)
+  test_algorithm_pattern_fails("last_hit_creep")
 end
 
 function test_min_hp_enemy_building_succeed()
-  test_RefreshBot()
-
   UNIT_CAN_BE_SEEN = true
   UNIT_NO_NEARBY_UNITS = false
 
-  local desire, target = algorithms.min_hp_enemy_building(
-    GetBot(),
-    1200)
-
-  luaunit.assertEquals(desire, true)
-  luaunit.assertEquals(target:GetUnitName(), "tower1")
+  test_algorithm_pattern_succeed("min_hp_enemy_building", "tower1")
 end
 
 function test_min_hp_enemy_building_no_unit_fails()
-  test_RefreshBot()
-
   UNIT_CAN_BE_SEEN = true
   UNIT_NO_NEARBY_UNITS = true
 
-  local desire, target = algorithms.min_hp_enemy_building(
-    GetBot(),
-    1200)
-
-  luaunit.assertEquals(desire, false)
-  luaunit.assertEquals(target, nil)
+  test_algorithm_pattern_fails("min_hp_enemy_building")
 end
 
 function test_low_hp_enemy_hero_succeed()
-  test_RefreshBot()
-
   UNIT_CAN_BE_SEEN = true
   UNIT_NO_NEARBY_UNITS = false
 
-  local desire, target = algorithms.low_hp_enemy_hero(
-    GetBot(),
-    1200)
-
-  luaunit.assertEquals(desire, true)
-  luaunit.assertEquals(target:GetUnitName(), "unit1")
+  test_algorithm_pattern_succeed("low_hp_enemy_hero", "unit1")
 end
 
 function test_low_hp_enemy_hero_no_unit_fails()
-  test_RefreshBot()
-
   UNIT_CAN_BE_SEEN = true
   UNIT_NO_NEARBY_UNITS = true
 
-  local desire, target = algorithms.low_hp_enemy_hero(
-    GetBot(),
-    1200)
-
-  luaunit.assertEquals(desire, false)
-  luaunit.assertEquals(target, nil)
+  test_algorithm_pattern_fails("low_hp_enemy_hero")
 end
 
 function test_low_hp_enemy_building_succeed()
-  test_RefreshBot()
-
   UNIT_CAN_BE_SEEN = true
   UNIT_IS_NEARBY_TOWERS = true
   UNIT_NO_NEARBY_UNITS = false
 
-  local desire, target = algorithms.low_hp_enemy_building(
-    GetBot(),
-    1200)
-
-  luaunit.assertEquals(desire, true)
-  luaunit.assertEquals(target:GetUnitName(), "tower1")
+  test_algorithm_pattern_succeed("low_hp_enemy_building", "tower1")
 end
 
 function test_low_hp_enemy_building_no_unit_fails()
-  test_RefreshBot()
-
   UNIT_CAN_BE_SEEN = true
   UNIT_IS_NEARBY_TOWERS = true
   UNIT_NO_NEARBY_UNITS = true
 
-  local desire, target = algorithms.low_hp_enemy_building(
-    GetBot(),
-    1200)
-
-  luaunit.assertEquals(desire, false)
-  luaunit.assertEquals(target, nil)
+  test_algorithm_pattern_fails("low_hp_enemy_building")
 end
 
 os.exit(luaunit.LuaUnit.run())
