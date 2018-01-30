a33db43e3622ec5458c28b1416b67de7c3b0c731
Ilya Shpigor <petrsum@gmail.com>
2018-01-30 20:58:30 +0100

    Initial add the mode_retreat_generic.lua script

---
+++ bots/mode_retreat_generic.lua
@@ -0,0 +1,18 @@
+local functions = require(
+  GetScriptDirectory() .."/utility/functions")
+
+local constants = require(
+  GetScriptDirectory() .."/utility/constants")
+
+local M = {}
+
+function GetDesire()
+  -- TOOD: Implement this function
+  return 0
+end
+
+function Think()
+  -- TOOD: Implement this function
+end
+
+return M
