842a0bed560e9ac458ec33f6cfae9dc26f391fac
Ilya Shpigor <petrsum@gmail.com>
2018-02-03 11:37:32 +0100

    Add unit tests

--- bots/tests/mode_push_test.lua
+++ bots/tests/mode_push_test.lua
@@ -7,13 +7,23 @@ local mode_push = require("mode_push")
 local constants = require("constants")
 local luaunit = require('luaunit')
 
-function test_Think()
+function test_Think_attack_succeed()
   test_RefreshBot()
 
   ATTACK_TARGET = nil
   mode_push.Think(LANE_TOP)
 
   luaunit.assertNotEquals(ATTACK_TARGET, nil)
+end
+
+function test_Think_move_succeed()
+  test_RefreshBot()
+
+  UNIT_NO_NEARBY_UNITS = true
+  UNIT_MOVE_LOCATION = nil
+  mode_push.Think(LANE_TOP)
+
+  luaunit.assertEquals(UNIT_MOVE_LOCATION, {10, 10})
 end
 
 os.exit(luaunit.LuaUnit.run())
