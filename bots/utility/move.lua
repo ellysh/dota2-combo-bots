a04c1a5a73b7cf20bd0d9b12fff87a9cf76cffd7
Ilya Shpigor <petrsum@gmail.com>
2018-02-03 14:33:17 +0100

    Use a TP scroll in the Move function

--- bots/utility/move.lua
+++ bots/utility/move.lua
@@ -1,7 +1,23 @@
 local M = {}
 
+local function GetTpScrollAbility(unit)
+  return unit:GetAbilityByName("item_tpscroll")
+end
+
+local function CanUseTpScroll(unit, target_location)
+  return GetTpScrollAbility(unit):IsFullyCastable()
+         and constants.MIN_TELEPORT_RADIUS
+             < GetUnitToLocationDistance(unit, target_location)
+end
+
 function M.Move(unit, target_location)
-  unit:Action_MoveToLocation(target_location);
+  if CanUseTpScroll(unit, target_location) then
+    unit:Action_UseAbilityOnLocation(
+      GetTpScrollAbility(unit),
+      target_location)
+  else
+    unit:Action_MoveToLocation(target_location);
+  end
 end
 
 return M
