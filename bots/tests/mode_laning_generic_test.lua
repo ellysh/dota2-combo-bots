6457380e15df9f30424478835be9bfa0a9e31312
Ilya Shpigor <petrsum@gmail.com>
2018-02-17 16:42:46 +0100

    Add unit tests

--- bots/tests/mode_laning_generic_test.lua
+++ bots/tests/mode_laning_generic_test.lua
@@ -6,10 +6,19 @@ require("global_functions")
 local mode = require("mode_laning_generic")
 local luaunit = require("luaunit")
 
-function test_GetDesire_positive()
+function test_GetDesire_below_level_six_positive()
   test_RefreshBot()
 
   luaunit.assertEquals(GetDesire(), 0.5)
+end
+
+function test_GetDesire_after_level_six_positive()
+  test_RefreshBot()
+
+  local bot = GetBot()
+  bot.level = 7
+
+  luaunit.assertEquals(GetDesire(), 0.25)
 end
 
 os.exit(luaunit.LuaUnit.run())
