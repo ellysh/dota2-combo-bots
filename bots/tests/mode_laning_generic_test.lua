c0f18a897bc23457dd7e3aed53984b496ec75502
Ilya Shpigor <petrsum@gmail.com>
2018-02-17 16:27:49 +0100

    Add unit tests for the mode laning script

---
+++ bots/tests/mode_laning_generic_test.lua
@@ -0,0 +1,15 @@
+package.path = package.path .. ";../?.lua"
+
+pcall(require, "luacov")
+require("global_functions")
+
+local mode = require("mode_laning_generic")
+local luaunit = require("luaunit")
+
+function test_GetDesire_positive()
+  test_RefreshBot()
+
+  luaunit.assertEquals(GetDesire(), 0)
+end
+
+os.exit(luaunit.LuaUnit.run())
