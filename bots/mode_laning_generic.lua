56b5cacfd837ba079e743f668134b3a25f2821fe
Ilya Shpigor <petrsum@gmail.com>
2018-02-17 16:41:27 +0100

    Limit the laning desire by the maximum value

--- bots/mode_laning_generic.lua
+++ bots/mode_laning_generic.lua
@@ -1,6 +1,14 @@
+local functions = require(
+  GetScriptDirectory() .."/utility/functions")
+
+local constants = require(
+  GetScriptDirectory() .."/utility/constants")
+
 local player_desires = require(
   GetScriptDirectory() .."/utility/player_desires")
 
 function GetDesire()
-  return player_desires.GetDesire("BOT_MODE_LANING")
+  return functions.GetNormalizedDesire(
+    player_desires.GetDesire("BOT_MODE_LANING"),
+    constants.MAX_LANING_DESIRE)
 end
