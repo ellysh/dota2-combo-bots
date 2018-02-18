a6786e6b68a8546d77709ef8a745f3033108aa12
Ilya Shpigor <petrsum@gmail.com>
2018-02-18 13:43:01 +0100

    Remove item builds for unsupported heroes

--- bots/database/item_build.lua
+++ bots/database/item_build.lua
@@ -88,142 +88,6 @@ M.ITEM_BUILD = {
       "item_lotus_orb"}
   },
 
-  npc_dota_hero_venomancer = {
-    items = {
-      "item_tango",
-      "item_wraith_band",
-      "nil",
-      "item_boots",
-      "nil",
-      "item_boots_of_elves",
-      "item_power_treads",
-      "item_ring_of_aquila",
-      "item_magic_wand",
-      "item_veil_of_discord",
-      "item_hurricane_pike",
-      "item_ultimate_scepter",
-      "item_shivas_guard"}
-  },
-
-  npc_dota_hero_obsidian_destroyer = {
-    items = {
-      "item_tango",
-      "item_null_talisman",
-      "nil",
-      "nil",
-      "item_boots",
-      "item_robe",
-      "item_power_treads",
-      "item_hurricane_pike",
-      "item_blink",
-      "item_shivas_guard",
-      "item_moon_shard",
-      "item_black_king_bar",
-      "item_octarine_core"}
-  },
-
-  npc_dota_hero_death_prophet = {
-    items = {
-      "item_tango",
-      "item_null_talisman",
-      "nil",
-      "nil",
-      "nil",
-      "nil",
-      "item_bottle",
-      "item_phase_boots",
-      "item_cyclone",
-      "item_octarine_core",
-      "item_shivas_guard",
-      "item_heart",
-      "item_black_king_bar"}
-  },
-
-  npc_dota_hero_queenofpain = {
-    items = {
-      "item_tango",
-      "item_null_talisman",
-      "nil",
-      "item_bottle",
-      "item_boots",
-      "item_robe",
-      "item_power_treads",
-      "nil",
-      "item_orchid",
-      "item_black_king_bar",
-      "item_shivas_guard",
-      "item_mjollnir",
-      "item_octarine_core"}
-  },
-
-  npc_dota_hero_bristleback = {
-    items = {
-      "item_tango",
-      "item_flask",
-      "item_stout_shield",
-      "item_magic_wand",
-      "item_boots",
-      "item_belt_of_strength",
-      "item_power_treads",
-      "nil",
-      "item_blade_mail",
-      "item_crimson_guard",
-      "item_radiance",
-      "item_heart",
-      "item_abyssal_blade"}
-  },
-
-  npc_dota_hero_tidehunter = {
-    items = {
-      "item_tango",
-      "item_flask",
-      "nil",
-      "nil",
-      "nil",
-      "nil",
-      "item_magic_wand",
-      "item_arcane_boots",
-      "item_blink",
-      "item_mekansm",
-      "item_refresher",
-      "item_shivas_guard",
-      "item_heavens_halberd"}
-  },
-
-  npc_dota_hero_shredder = {
-    items = {
-      "item_tango",
-      "item_stout_shield",
-      "nil",
-      "nil",
-      "nil",
-      "nil",
-      "item_magic_wand",
-      "item_soul_ring",
-      "item_arcane_boots",
-      "item_bloodstone",
-      "item_shivas_guard",
-      "item_black_king_bar",
-      "item_octarine_core"}
-  },
-
-  npc_dota_hero_skywrath_mage = {
-    items = {
-      "item_tango",
-      "item_clarity",
-      "nil",
-      "nil",
-      "nil",
-      "nil",
-      "item_magic_wand",
-      "item_arcane_boots",
-      "item_rod_of_atos",
-      "item_kaya",
-      "item_aether_lens",
-      "item_force_staff",
-      "item_ultimate_scepter"}
-  },
-
   npc_dota_hero_lion = {
     items = {
       "item_tango",
@@ -239,23 +103,6 @@ M.ITEM_BUILD = {
       "item_glimmer_cape",
       "item_ultimate_scepter",
       "item_force_staff"}
-  },
-
-  npc_dota_hero_sand_king = {
-    items = {
-      "item_tango",
-      "item_stout_shield",
-      "nil",
-      "nil",
-      "nil",
-      "nil",
-      "item_magic_wand",
-      "item_tranquil_boots",
-      "item_blink",
-      "item_force_staff",
-      "item_cyclone",
-      "item_lotus_orb",
-      "item_ultimate_scepter"}
   },
 
   npc_dota_hero_lich = {
@@ -275,108 +122,6 @@ M.ITEM_BUILD = {
       "item_rod_of_atos"}
   },
 
-  npc_dota_hero_faceless_void = {
-    items = {
-      "item_tango",
-      "item_flask",
-      "item_stout_shield",
-      "nil",
-      "nil",
-      "nil",
-      "item_power_treads",
-      "item_mask_of_madness",
-      "item_invis_sword",
-      "item_manta",
-      "item_monkey_king_bar",
-      "item_mjollnir",
-      "item_butterfly"}
-  },
-
-  npc_dota_hero_nevermore = {
-    items = {
-      "item_wraith_band",
-      "item_branches",
-      "item_tango",
-      "item_bottle",
-      "item_boots",
-      "item_boots_of_elves",
-      "item_power_treads",
-      "nil",
-      "item_ring_of_aquila",
-      "item_invis_sword",
-      "item_hurricane_pike",
-      "item_butterfly",
-      "item_orchid"}
-  },
-
-  npc_dota_hero_tiny = {
-    items = {
-      "item_tango",
-      "item_flask",
-      "item_stout_shield",
-      "item_magic_wand",
-      "item_boots",
-      "item_belt_of_strength",
-      "item_power_treads",
-      "nil",
-      "item_invis_sword",
-      "item_black_king_bar",
-      "item_assault",
-      "item_orchid",
-      "item_heart"}
-  },
-
-  npc_dota_hero_dazzle = {
-    items = {
-      "item_tango",
-      "item_clarity",
-      "nil",
-      "nil",
-      "nil",
-      "nil",
-      "item_magic_wand",
-      "item_arcane_boots",
-      "item_solar_crest",
-      "item_force_staff",
-      "item_glimmer_cape",
-      "item_aether_lens",
-      "item_ultimate_scepter"}
-  },
-
-  npc_dota_hero_lina = {
-    items = {
-      "item_tango",
-      "item_clarity",
-      "nil",
-      "nil",
-      "nil",
-      "nil",
-      "item_magic_wand",
-      "item_arcane_boots",
-      "item_cyclone",
-      "item_invis_sword",
-      "item_force_staff",
-      "item_sheepstick",
-      "item_ultimate_scepter"}
-  },
-
-  npc_dota_hero_legion_commander = {
-    items = {
-      "item_tango",
-      "item_flask",
-      "item_stout_shield",
-      "nil",
-      "nil",
-      "nil",
-      "item_magic_wand",
-      "item_phase_boots",
-      "item_blink",
-      "item_blade_mail",
-      "item_black_king_bar",
-      "item_assault",
-      "item_abyssal_blade"}
-  },
-
   npc_dota_hero_drow_ranger = {
     items = {
       "item_tango",
@@ -392,23 +137,6 @@ M.ITEM_BUILD = {
       "item_butterfly",
       "item_mjollnir",
       "nil"}
-  },
-
-  npc_dota_hero_razor = {
-    items = {
-      "item_tango",
-      "item_wraith_band",
-      "nil",
-      "nil",
-      "nil",
-      "nil",
-      "item_ring_of_aquila",
-      "item_phase_boots",
-      "item_ancient_janggo",
-      "item_sange_and_yasha",
-      "item_black_king_bar",
-      "item_hurricane_pike",
-      "item_butterfly"}
   },
 
   npc_dota_hero_skeleton_king = {
@@ -443,23 +171,6 @@ M.ITEM_BUILD = {
       "item_invis_sword",
       "item_greater_crit",
       "item_butterfly"}
-  },
-
-  npc_dota_hero_lion = {
-    items = {
-      "item_tango",
-      "item_clarity",
-      "nil",
-      "item_branches",
-      "item_boots",
-      "item_magic_wand",
-      "item_tranquil_boots",
-      "item_blink",
-      "item_aether_lens",
-      "nil",
-      "item_force_staff",
-      "item_ultimate_scepter",
-      "item_cyclone"}
   },
 
   npc_dota_hero_phantom_assassin = {
