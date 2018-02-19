f35dda495b91964fda520d987d31981168efa9a2
Ilya Shpigor <petrsum@gmail.com>
2018-02-19 20:55:47 +0100

    Fix a mistake in the script name

--- bots/mode_defend_tower_bot_generic.lua
+++ bots/mode_defend_tower_bot_generic.lua
@@ -8,7 +8,6 @@ local player_desires = require(
   GetScriptDirectory() .."/utility/player_desires")
 
 function GetDesire()
-  print("defend_tower_bottom() - GetDesire()")
   return functions.GetNormalizedDesire(
            GetDefendLaneDesire(LANE_BOT)
            + player_desires.GetDesire("BOT_MODE_DEFEND_TOWER_BOT"),
