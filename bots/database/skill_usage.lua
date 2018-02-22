f8b93467aedd0f4a52dd12b268bb7f19928cac81
Ilya Shpigor <petrsum@gmail.com>
2018-02-22 20:14:02 +0100

    Add usage of the Cheese item

--- bots/database/skill_usage.lua
+++ bots/database/skill_usage.lua
@@ -1062,6 +1062,22 @@ M.SKILL_USAGE = {
       BOT_MODE_EVASIVE_MANEUVERS  = {algorithms["nil"], nil}
   },
 
+  item_cheese = {
+      any_mode = {algorithms["low_hp_self"], 90},
+      team_fight = {algorithms["nil"], nil},
+      BOT_MODE_ROAM = {algorithms["nil"], nil},
+      BOT_MODE_TEAM_ROAM = {algorithms["nil"], nil},
+      BOT_MODE_PUSH_TOWER = {algorithms["nil"], nil},
+      BOT_MODE_ATTACK = {algorithms["nil"], nil},
+      BOT_MODE_LANING = {algorithms["nil"], nil},
+      BOT_MODE_ROSHAN = {algorithms["nil"], nil},
+      BOT_MODE_FARM = {algorithms["nil"], nil},
+      BOT_MODE_DEFEND_TOWER = {algorithms["nil"], nil},
+      BOT_MODE_DEFEND_ALLY = {algorithms["nil"], nil},
+      BOT_MODE_RETREAT = {algorithms["nil"], nil},
+      BOT_MODE_EVASIVE_MANEUVERS  = {algorithms["nil"], nil}
+  },
+
 }
 
 return M
