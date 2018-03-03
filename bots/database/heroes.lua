41a0bee6c7cd10e4a3263d23cb4321d88f2eec13
Ilya Shpigor <petrsum@gmail.com>
2018-03-03 10:03:44 +0100

    Add extra information about hero in the HEROES table

--- bots/database/heroes.lua
+++ bots/database/heroes.lua
@@ -6,196 +6,196 @@ M.HEROES = {
   npc_dota_hero_phantom_assassin = {
     position = {1, 2},
     combo_heroes = {
-      "npc_dota_hero_ogre_magi",
-      "npc_dota_hero_omniknight",
-      "npc_dota_hero_shadow_shaman"
+      "agility",
+      "physical",
+      "melee"
     },
     counter_heroes = {
-      "npc_dota_hero_axe",
-      "npc_dota_hero_dragon_knight",
-      "npc_dota_hero_sand_king"
+      "blink",
+      "attack_damage",
+      "slow"
     }
   },
 
   npc_dota_hero_chaos_knight = {
     position = {1, 2},
     combo_heroes = {
-      "npc_dota_hero_crystal_maiden",
-      "npc_dota_hero_wisp",
-      "npc_dota_hero_rattletrap"
+      "strength",
+      "physical",
+      "melee"
     },
     counter_heroes = {
-      "npc_dota_hero_sven",
-      "npc_dota_hero_medusa",
-      "npc_dota_hero_bristleback"
+      "blink",
+      "stun",
+      "attack_damage"
     }
   },
 
   npc_dota_hero_crystal_maiden = {
     position = {4, 5},
     combo_heroes = {
-      "npc_dota_hero_juggernaut",
-      "npc_dota_hero_sven",
-      "npc_dota_hero_ursa"
+      "intelligence",
+      "magical",
+      "ranged"
     },
     counter_heroes = {
-      "npc_dota_hero_elder_titan",
-      "npc_dota_hero_earth_spirit",
-      "npc_dota_hero_brewmaster"
+      "stun",
+      "slow",
+      "aoe"
     }
   },
 
   npc_dota_hero_drow_ranger = {
     position = {1, 2},
     combo_heroes = {
-      "npc_dota_hero_vengefulspirit",
-      "npc_dota_hero_visage",
-      "npc_dota_hero_windrunner"
+      "agility",
+      "physical",
+      "ranged"
     },
     counter_heroes = {
-      "npc_dota_hero_phantom_assassin",
-      "npc_dota_hero_phantom_lancer",
-      "npc_dota_hero_broodmother"
+      "silence",
+      "slow",
+      "nil"
     }
   },
 
   npc_dota_hero_juggernaut = {
     position = {1, 3},
     combo_heroes = {
-      "npc_dota_hero_crystal_maiden",
-      "npc_dota_hero_shadow_shaman",
-      "npc_dota_hero_venomancer"
+      "agility",
+      "physical",
+      "melee"
     },
     counter_heroes = {
-      "npc_dota_hero_ursa",
-      "npc_dota_hero_razor",
-      "npc_dota_hero_drow_ranger"
+      "aoe",
+      "attack_damage",
+      "invulnerable"
     }
   },
 
   npc_dota_hero_lich = {
     position = {5, 4},
     combo_heroes = {
-      "npc_dota_hero_faceless_void",
-      "npc_dota_hero_weaver",
-      "npc_dota_hero_obsidian_destroyer"
+      "intelligence",
+      "magical",
+      "ranged"
     },
     counter_heroes = {
-      "npc_dota_hero_sand_king",
-      "npc_dota_hero_weaver",
-      "npc_dota_hero_broodmother"
+      "slow",
+      "mana_regen",
+      "nuke"
     }
   },
 
   npc_dota_hero_lion = {
     position = {5, 4},
     combo_heroes = {
-      "npc_dota_hero_lina",
-      "npc_dota_hero_legion_commander",
-      "npc_dota_hero_juggernaut"
+      "intelligence",
+      "magical",
+      "ranged"
     },
     counter_heroes = {
-      "npc_dota_hero_vengefulspirit",
-      "npc_dota_hero_centaur",
-      "npc_dota_hero_tidehunter"
+      "stun",
+      "hex",
+      "mana_regen"
     }
   },
 
   npc_dota_hero_phantom_lancer = {
     position = {1, 3},
     combo_heroes = {
-      "npc_dota_hero_keeper_of_the_light",
-      "npc_dota_hero_dazzle",
-      "npc_dota_hero_lich"
+      "agility",
+      "physical",
+      "melee"
     },
     counter_heroes = {
-      "npc_dota_hero_axe",
-      "npc_dota_hero_sand_king",
-      "npc_dota_hero_sven"
+      "illusions",
+      "attack_damage",
+      "nil"
     }
   },
 
   npc_dota_hero_shadow_shaman = {
     position = {5, 4},
     combo_heroes = {
-      "npc_dota_hero_ursa",
-      "npc_dota_hero_juggernaut",
-      "npc_dota_hero_broodmother"
+      "intelligence",
+      "magical",
+      "ranged"
     },
     counter_heroes = {
-      "npc_dota_hero_drow_ranger",
-      "npc_dota_hero_phantom_lancer",
-      "npc_dota_hero_terrorblade"
+      "hex",
+      "stun",
+      "minions"
     }
   },
 
   npc_dota_hero_skeleton_king = {
     position = {2, 3},
     combo_heroes = {
-      "npc_dota_hero_vengefulspirit",
-      "npc_dota_hero_lina",
-      "npc_dota_hero_ursa"
+      "strength",
+      "physical",
+      "melee"
     },
     counter_heroes = {
-      "npc_dota_hero_phantom_lancer",
-      "npc_dota_hero_undying",
-      "npc_dota_hero_broodmother"
+      "attack_damage",
+      "stun",
+      "nil"
     }
   },
 
   npc_dota_hero_sniper = {
     position = {2, 1},
     combo_heroes = {
-      "npc_dota_hero_ogre_magi",
-      "npc_dota_hero_skeleton_king",
-      "npc_dota_hero_vengefulspirit"
+      "agility",
+      "physical",
+      "ranged"
     },
     counter_heroes = {
-      "npc_dota_hero_phantom_assassin",
-      "npc_dota_hero_rattletrap",
-      "npc_dota_hero_centaur"
+      "attack_damage",
+      "slow",
+      "aoe"
     }
   },
 
   npc_dota_hero_sven = {
     position = {1, 3},
     combo_heroes = {
-      "npc_dota_hero_crystal_maiden",
-      "npc_dota_hero_vengefulspirit",
-      "npc_dota_hero_wisp"
+      "strength",
+      "physical",
+      "melee"
     },
     counter_heroes = {
-      "npc_dota_hero_razor",
-      "npc_dota_hero_shredder",
-      "npc_dota_hero_necrolyte"
+      "attack_damage",
+      "stun",
+      "aoe"
     }
   },
 
   npc_dota_hero_ursa = {
     position = {3, 1},
     combo_heroes = {
-      "npc_dota_hero_crystal_maiden",
-      "npc_dota_hero_shadow_shaman",
-      "npc_dota_hero_wisp"
+      "agility",
+      "physical",
+      "melee"
     },
     counter_heroes = {
-      "npc_dota_hero_razor",
-      "npc_dota_hero_venomancer",
-      "npc_dota_hero_phantom_lancer"
+      "attack_damage",
+      "slow",
+      "armor"
     }
   },
 
   npc_dota_hero_warlock = {
     position = {2, 4},
     combo_heroes = {
-      "npc_dota_hero_beastmaster",
-      "npc_dota_hero_lycan",
-      "npc_dota_hero_abaddon"
+      "intelligence",
+      "magical",
+      "ranged"
     },
     counter_heroes = {
-      "npc_dota_hero_antimage",
-      "npc_dota_hero_lycan",
-      "npc_dota_hero_abaddon"
+      "stun",
+      "slow",
+      "minions"
     }
   },
 
